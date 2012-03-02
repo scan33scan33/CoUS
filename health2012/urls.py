a544370bccc74ff993128767ac030f60a4969b00
David Huang <ymcagodme@gmail.com>
2012-03-02 11:46:57 -0800

    Frontend: UI modified
    Adjust the button's effect.
    03/02 DEMO version

--- health2012/urls.py
+++ health2012/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+import os
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
