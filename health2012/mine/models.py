ab3976f4022628703061c16cdbb306e08821a721
scan33scan33 <scan33scan33@gmail.com>
2012-03-02 22:06:42 -0500

    Database setup

--- health2012/mine/models.py
+++ health2012/mine/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    topic = models.CharField(max_length=50)
+    subtopic = models.CharField(max_length=50)
+    attr = models.CharField(max_length=50) #All attributes, comma separated
+
+    race = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    education = models.CharField(max_length=30)
+    age = models.CharField(max_length=30)
+    
+    value = models.FloatField() 
